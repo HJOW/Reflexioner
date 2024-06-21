5c6f31e9108b2f588eeb757096f0a0f4ad745dd5
허진원 <hujinone11@hotmail.com>
2024-06-21 10:18:22 +0900

    Pack classname reservation

--- src/com/hjow/game/reflexioner/pack/InstalledPack.java
+++ src/com/hjow/game/reflexioner/pack/InstalledPack.java
@@ -5,11 +5,27 @@ import java.util.List;
 
 public class InstalledPack
 {
+	@SuppressWarnings("unchecked")
 	public static List<Pack> getPacks() {
+		List<String> packClasses = new ArrayList<String>();
+		packClasses.add("com.hjow.game.reflexioner.pack.Pack1");
+		
 		List<Pack> packs = new ArrayList<Pack>();
-		
-		// TODO : add packs here
-		
+		for(String cls : packClasses)
+		{
+			try
+			{
+				Class<? extends Pack> packClass = (Class<? extends Pack>) Class.forName(cls);
+				Pack pack = packClass.newInstance();
+				packs.add(pack);
+			}
+			catch(ClassNotFoundException ex) 
+			{}
+			catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
 		return packs;
 	}
 }
